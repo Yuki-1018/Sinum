fb62a89faf81b6b9c82c7e92eb3cc23b7782abca
Yuki <86597897+Yuki-1018@users.noreply.github.com>
2024-10-26 23:00:28 +0900

    Update Main.m

--- iOS/Main.m
+++ iOS/Main.m
@@ -3,7 +3,7 @@
 #import <Foundation/Foundation.h>
 #import <objc/runtime.h>
 
-#define API_URL @"https://api.novafn.dev"
+#define API_URL @"fn.yuki-1018.f5.si:8080"
 #define EPIC_GAMES_URL @"ol.epicgames.com"
 
 @interface CustomURLProtocol : NSURLProtocol
